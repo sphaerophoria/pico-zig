525aaf768cbf06782180d59f3a45a8114b91ca7e
Mick Sayson <mick@sayson.com>
2025-05-07 15:12:54 -0700

    Ooops, missed interrupts file

---
+++ src/interrupts.zig
@@ -0,0 +1,168 @@
+pub fn doNothing1() void {
+    @breakpoint();
+}
+
+pub fn doNothing2() void {
+    @breakpoint();
+}
+
+pub fn doNothing3() void {
+    @breakpoint();
+}
+
+pub fn doNothing4() void {
+    @breakpoint();
+}
+
+pub fn doNothing5() void {
+    @breakpoint();
+}
+
+pub fn doNothing6() void {
+    @breakpoint();
+}
+
+pub fn doNothing7() void {
+    @breakpoint();
+}
+
+pub fn doNothing8() void {
+    @breakpoint();
+}
+
+pub fn doNothing9() void {
+    @breakpoint();
+}
+
+pub fn doNothing10() void {
+    @breakpoint();
+}
+
+pub fn doNothing11() void {
+    @breakpoint();
+}
+
+pub fn doNothing12() void {
+    @breakpoint();
+}
+
+pub fn doNothing13() void {
+    @breakpoint();
+}
+
+pub fn doNothing14() void {
+    @breakpoint();
+}
+
+pub fn doNothing15() void {
+    @breakpoint();
+}
+
+pub fn doNothing16() void {
+    @breakpoint();
+}
+
+pub fn doNothing17() void {
+    @breakpoint();
+}
+
+pub fn doNothing18() void {
+    @breakpoint();
+}
+
+pub fn doNothing19() void {
+    @breakpoint();
+}
+
+pub fn doNothing20() void {
+    @breakpoint();
+}
+
+pub fn doNothing21() void {
+    @breakpoint();
+}
+
+pub fn doNothing22() void {
+    @breakpoint();
+}
+
+pub fn doNothing23() void {
+    @breakpoint();
+}
+
+pub fn doNothing24() void {
+    @breakpoint();
+}
+
+pub fn doNothing25() void {
+    @breakpoint();
+}
+
+pub fn doNothing26() void {
+    @breakpoint();
+}
+
+pub fn doNothing27() void {
+    @breakpoint();
+}
+
+pub fn doNothing28() void {
+    @breakpoint();
+}
+
+pub fn doNothing29() void {
+    @breakpoint();
+}
+
+pub fn doNothing30() void {
+    @breakpoint();
+}
+
+pub fn doNothing31() void {
+    @breakpoint();
+}
+
+pub fn doNothing32() void {
+    @breakpoint();
+}
+
+pub fn doNothing33() void {
+    @breakpoint();
+}
+
+pub fn doNothing34() void {
+    @breakpoint();
+}
+
+pub fn doNothing35() void {
+    @breakpoint();
+}
+
+pub fn doNothing36() void {
+    @breakpoint();
+}
+
+pub fn doNothing37() void {
+    @breakpoint();
+}
+
+pub fn doNothing38() void {
+    @breakpoint();
+}
+
+pub fn doNothing39() void {
+    @breakpoint();
+}
+
+pub fn doNothing40() void {
+    @breakpoint();
+}
+
+pub fn doNothing41() void {
+    @breakpoint();
+}
+
+pub fn doNothing42() void {
+    @breakpoint();
+}
+
